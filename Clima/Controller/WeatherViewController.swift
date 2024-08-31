ee3fb2b014fdcfdb8adce3f25a7ace2855b055dc
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-31 18:31:17 +0100

    get the input of text field

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.delegate = self
     }
 
-
+    @IBAction func searchPresssed(_ sender: UIButton) {
+        textField.endEditing(true)
+        print(textField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        print(textField.text!)
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            return true
+        }else{
+            textField.placeholder = "Type something!"
+            return false
+        }
+    }
+    
 }
 
